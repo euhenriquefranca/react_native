eef7ebea503d8477826f86441be5e5585022641b
Henrique França <euhenriquefranca@gmail.com>
2020-10-26 21:53:43 -0300

    Usando Hooks useState e useEfect

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Alert, Button, Text, View } from "react-native";
+import { css } from "./assets/css/Css";
+import Page from "./views/Page";
 
 export default function App() {
+  const [product, setProduct] = useState("bola");
+  const [quantity, setQuantity] = useState(0);
+
+  //substitui o component DidMount
+  useEffect(() => { // Hooks useEffect = trabalhar com efeitos colaterais
+    if(quantity > 0){
+      Alert.alert('Novo produto foi adicionado')
+    }
+  }, [quantity]);
+  const props = {
+    empresa: "Webdesign em Foco",
+    name: "Thiago",
+    produto: product,
+    quantidade: quantity
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={css.container}>
+      <Text>{product}</Text>
+      <Page {...props} />
+      <Button title='Adicionar produtos' onPress={()=> setQuantity(quantity + 1)} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
