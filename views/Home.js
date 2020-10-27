d3834ef7e9df87d5650c5efeba2713a11c88a043
Henrique França <euhenriquefranca@gmail.com>
2020-10-27 19:59:49 -0300

    Navegaçao entre telas

---
+++ views/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View, Button } from "react-native";
+
+export default function Home(props) {
+  console.log(props);
+  return (
+    <View>
+      <Text>Esse é o componente Home</Text>
+      <Button
+        title="Ir para o Login"
+        onPress={() =>
+          props.navigation.navigate("Login", {
+            id: 30,
+          })
+        }
+      />
+    </View>
+  );
+}
